94581ba627a73d3e6dadf674d06113b6639accad
AsherDoesTechs <Asherbasco93@gmail.com>
2025-06-24 17:27:42 +0800

    Login API working now

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,3 +1,13 @@
-import { handleLogin } from '@auth0/nextjs-auth0/edge';
+import { NextResponse } from 'next/server';
 
-export const GET = handleLogin();
+export async function GET() {
+  const auth0LoginUrl = `${process.env.AUTH0_ISSUER_BASE_URL}/authorize?` +
+    new URLSearchParams({
+      client_id: process.env.AUTH0_CLIENT_ID!,
+      redirect_uri: `${process.env.AUTH0_BASE_URL}/api/auth/callback`,
+      response_type: 'code',
+      scope: 'openid profile email',
+    });
+
+  return NextResponse.redirect(auth0LoginUrl);
+}
